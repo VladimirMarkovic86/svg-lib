0793e1ef617d4c032978877be6b33afa8d88612f
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:55:21 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,20 +1,22 @@
-(defproject org.clojars.vladimirmarkovic86/svg-lib "0.1.9"
+(defproject org.clojars.vladimirmarkovic86/svg-lib "0.1.10"
   :description "Scalable Vector Graphics library"
   :url "http://github.com/VladimirMarkovic86/svg-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.7"]
-                 [org.clojars.vladimirmarkovic86/js-lib "0.1.17"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
+                 [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.8"]
+                 [org.clojars.vladimirmarkovic86/js-lib "0.1.18"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/cljs"]
   
-  :jar-exclusions [#"^public/"]
+  :jar-exclusions [#"^public/"
+                   #"README.md$"
+                   #"LICENSE$"]
 
   :plugins [[lein-cljsbuild  "1.1.7"]
             [lein-doo "0.1.11"]
