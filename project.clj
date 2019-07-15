7a17034b88f58a410e2fa0bde86edd4de1c2fde6
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:17:43 +0200

    Task 88: Excluded public folder from some more clojurescripts projects packing jar file.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/svg-lib "0.1.8"
+(defproject org.clojars.vladimirmarkovic86/svg-lib "0.1.9"
   :description "Scalable Vector Graphics library"
   :url "http://github.com/VladimirMarkovic86/svg-lib"
   :license {:name "Eclipse Public License"
@@ -13,6 +13,8 @@
   :min-lein-version "2.0.0"
   
   :source-paths ["src/cljs"]
+  
+  :jar-exclusions [#"^public/"]
 
   :plugins [[lein-cljsbuild  "1.1.7"]
             [lein-doo "0.1.11"]
